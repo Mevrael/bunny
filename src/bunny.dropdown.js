dd557d1a12ba1f01691019de0044edc4bbff59e4
Mev-Rael <mevrael@gmail.com>
2016-03-16 17:49:55 +0200

    0.7.7: fix dropdown undefined

--- src/bunny.dropdown.js
+++ src/bunny.dropdown.js
@@ -3,21 +3,25 @@ document.getElementsByClassName('dropdown').forEach((dropdown) => {
 
     let toggle_btn = dropdown.getElementsByClassName('dropdown-toggle')[0];
 
-    let body_handler = () => {
-        if (dropdown.classList.contains('open')) {
-            dropdown.classList.remove('open');
-            document.body.removeEventListener('click', body_handler);
-        }
-    };
+    if (toggle_btn !== undefined) {
 
-    toggle_btn.addEventListener('click', (e) => {
-        e.stopPropagation();
-        if (!dropdown.classList.contains('open')) {
-            dropdown.classList.add('open');
-            document.body.addEventListener('click', body_handler);
-        } else {
-            dropdown.classList.remove('open');
-        }
-    });
+        let body_handler = () => {
+            if (dropdown.classList.contains('open')) {
+                dropdown.classList.remove('open');
+                document.body.removeEventListener('click', body_handler);
+            }
+        };
+
+        toggle_btn.addEventListener('click', (e) => {
+            e.stopPropagation();
+            if (!dropdown.classList.contains('open')) {
+                dropdown.classList.add('open');
+                document.body.addEventListener('click', body_handler);
+            } else {
+                dropdown.classList.remove('open');
+            }
+        });
+
+    }
 
 });
