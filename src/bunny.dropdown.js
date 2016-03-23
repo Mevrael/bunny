d1c33528a135388235a2c41714b33941e514efb7
Mev-Rael <mevrael@gmail.com>
2016-03-23 16:26:46 +0200

    0.7.8: dropdown: add option: data-close-inside=false

--- src/bunny.dropdown.js
+++ src/bunny.dropdown.js
@@ -4,6 +4,13 @@ document.getElementsByClassName('dropdown').forEach((dropdown) => {
     let toggle_btn = dropdown.getElementsByClassName('dropdown-toggle')[0];
 
     if (toggle_btn !== undefined) {
+        
+        if (toggle_btn.getAttribute('data-close-inside') === 'false') {
+            let dropdown_menu = dropdown.getElementsByClassName('dropdown-menu')[0];
+            dropdown_menu.addEventListener('click', (e) => {
+                e.stopPropagation();
+            })
+        }
 
         let body_handler = () => {
             if (dropdown.classList.contains('open')) {
